e10e7bfdcbdf18f10ad19a1b2fea7a752c6129ab
S-North <stuart.north@gmail.com>
2021-03-17 22:04:29 +0000

    Function to get ability modifiers

--- tracker.py
+++ tracker.py
@@ -1,6 +1,6 @@
 import random  # we need this for all the random stuff we're going to do
 
-class participant():  # create a class so we can create people objects with all the properties we need
+class person():  # create a class so we can create people objects with all the properties we need
     def __init__(self, name, hp, ac, attack, damage, initiative, str, dex, con, wis, int, cha):
         self.name = name
         self.hp = hp
@@ -19,25 +19,38 @@ class participant():  # create a class so we can create people objects with all 
         return self.name
 
 
-participant1 = participant("Bob", 38, 14, 1, (1,4), 10, 10, 10, 10, 12, 5, 13)  # lets start creating some people :)
-participant2 = participant("Jerry", 14, 16, 1, (1,4), 10, 10, 10, 10, 12, 5, 13)
-participant3 = participant("Micky Mouse", 56, 17, 1, (1,4), 10, 10, 10, 10, 12, 5, 13)
-participant4 = participant("Alex", 76, 16, 1, (1,4), 10, 10, 10, 10, 12, 5, 13)
-participant5 = participant("Stuart", 3, 13, 1, (1,4), 10, 10, 10, 10, 12, 5, 13)
-participant6 = participant("Glastaff", 34, 16, 1, (1,4), 10, 10, 10, 10, 12, 5, 13)
+person1 = person("Goblin 1", 38, 14, 1, (1,4), 10, 10, 10, 10, 12, 5, 13)  # lets start creating some people :)
+person2 = person("Goblin 2", 14, 16, 1, (1,4), 10, 10, 10, 10, 12, 5, 13)
+person3 = person("Sherlock", 56, 17, 1, (1,4), 10, 10, 10, 10, 12, 5, 13)
+person4 = person("Polydeuces", 76, 16, 1, (1,4), 10, 14, 10, 10, 12, 5, 13)
+person5 = person("Alan", 3, 13, 1, (1,4), 10, 10, 10, 10, 12, 5, 13)
+person6 = person("Raskas", 34, 16, 1, (1,4), 10, 10, 10, 10, 12, 5, 13)
+person7 = person("Goblin 3", 14, 16, 1, (1,4), 10, 10, 10, 10, 12, 5, 13)
+person8 = person("Goblin 4", 14, 16, 1, (1,4), 10, 10, 10, 10, 12, 5, 13)
 
-participants = [participant1, participant2, participant3, participant4, participant5, participant6]  # put all the people in a list
+persons = [person1, person2, person3, person4, person5, person6, person7, person8]  # put all the people in a list
 
-participant1.name = "Erik" # play around with changing the properties of some of the people
-participant1.hp = participant1.hp - 10
+person1.name = "Erik" # play around with changing the properties of some of the people
+person1.hp = person1.hp - 10
 
-participant7 = participant("Sildar", 40, 17, 1, (1,8), 0, 14, 15, 10, 12, 5, 13)  # create another person
-participants.append(participant7)  # add them to the list
+def calc_modifier(ability_score):
+    if ability_score >= 10:
+        return int((ability_score - 10) / 2)
+    else:
+        dict = {"1": -5, "2": -4, "3": -4, "4": -3, "5": -3, "6": -2, "7": -2, "8": -1, "9": -1}  # had to fudge this as I couldn't find a good calculation
+        return dict[str(ability_score)]
 
-for participant in participants:  # loop over all the people in the list
-    participant.initiative = random.randint(1, 20)  # roll initiative for each one
 
-participants.sort(key=lambda x: x.initiative, reverse=True)  # sort the list into revese initiative order (using a highly confusing lambda function)
+for ability in range(1,31):
+    print(f"abilty {ability} gets you modifier {calc_modifier(ability)}")
 
-for participant in participants:  # loop over everything again to print out stuff
-    print(f"Initiative = {participant.initiative}".ljust(17), f"{participant.name}".ljust(16), f"HP: {participant.hp}, ".ljust(10), f"AC: {participant.ac}".ljust(10))
+person9 = person("Sildar", 40, 17, 1, (1,8), 0, 14, 15, 10, 12, 5, 13)  # create another person
+persons.append(person7)  # add them to the list
+
+for person in persons:  # loop over all the people in the list
+    person.initiative = random.randint(1, 20)  # roll initiative for each one
+
+persons.sort(key=lambda x: x.initiative, reverse=True)  # sort the list into revese initiative order (using a highly confusing lambda function)
+
+for person in persons:  # loop over everything again to print out stuff
+    print(f"Initiative = {person.initiative}".ljust(17), f"{person.name}".ljust(16), f"HP: {person.hp}, ".ljust(10), f"AC: {person.ac}".ljust(10))
